a5cc3e2fc40f0d9c3658bfac19065e077eb02750
Dan Rahmel <drahmel7@gmail.com>
2016-08-16 15:28:28 -0700

    DJR:Flexbox with colors

--- index.ios.js
+++ index.ios.js
@@ -2,33 +2,65 @@ var React = require('react-native');
 var {
   Text,
   View,
-  AppRegistry
+  AppRegistry,
 } = React;
 
 var StopWatch = React.createClass({
-  render: function() {
-    return <View>
-      <Text>
+  render: function () {
+    return <View style={styles.container}>
+      <View style={[styles.header, this.border('yellow')]}> // Yellow
+        <View style={this.border('red')}> // Red
+        <Text>
         00:00:00
-      </Text>
-      {this.startStopButton()}
-      {this.lapButton()}
-
+        </Text>
+        </View>
+        <View style={this.border('green')}> // Green
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
+      <View style={[styles.footer, this.border('blue')]}> // Blue
+        <Text>
+          I am a list of laps
+        </Text>
+      </View>
+    </View>
   },
-  startStopButton: function() {
+  startStopButton: function () {
     return <View>
       <Text>
         Start
       </Text>
     </View>
-
   },
-  lapButton: function() {
+  lapButton: function () {
     return <View>
-      <Text>
-        Lep
-      </Text>
+    <Text>
+    Lap
+    </Text>
     </View>
 
+  },
+  border: function(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
   }
 });
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignImages: 'stretch'
+  },
+  header: { // Yellow
+    flex: 1
+
+  },
+  footer: { // Blue
+    flex: 1
+  }
+})
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
